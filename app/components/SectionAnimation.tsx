cb24d048c6f3c66e7c101137a86343ef55c72650
Jakub Majerčík <89073674+iakupp@users.noreply.github.com>
2026-04-07 23:59:12 +0200

    motion repair

--- app/components/SectionAnimation.tsx
+++ app/components/SectionAnimation.tsx
@@ -1,22 +1,18 @@
 "use client";
 import { motion } from "framer-motion";
 
-const sectionVariants = {
-  hidden: { opacity: 0, y: 0 },
-  visible: { opacity: 1, y: 0, transition: { duration: .7, ease: "linear" } },
-};
-
 export const Section = ({ children }: { children: React.ReactNode }) => {
   return (
     <motion.div
-      className="my-20 w-full"  
-    //   variants={sectionVariants}
-      initial="hidden"
-      whileInView="visible"
-      viewport={{ once: true, amount: 0.3 }}  // animuje len keď sekcia 30%vstúpi do viewportu
+      className="my-20 w-full"
+      initial={{ opacity: 0, y: 20 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      viewport={{ once: true, amount: 0.3 }}
+      transition={{ duration: 0.6, ease: "easeOut" }}
     >
       {children}
     </motion.div>
   );
 };
+
 
